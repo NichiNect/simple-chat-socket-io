cf7d150886730e5261c8851a44b3f48ae36f4f9f
NichiNect <yoniwidhi2@gmail.com>
2023-05-12 10:19:05 +0700

    Update: implement the socket io event-listener & broadcast event

--- public/script.js
+++ public/script.js
@@ -2,6 +2,11 @@ const sendButton = document.querySelector('#send');
 const inputMessage = document.querySelector('#input');
 const displayChat = document.querySelector('.container-chat');
 
+const socketIO = io();
+socketIO.on('connect', () => {
+    console.log('client socket connected!');
+});
+
 const createBubbleChat = (chat) => {
     const newDivMessage = document.createElement('div');
     newDivMessage.classList.add('chat');
@@ -12,5 +17,16 @@ const createBubbleChat = (chat) => {
 sendButton.addEventListener('click', () => {
     const bubbleChat = createBubbleChat(inputMessage.value);
     displayChat.appendChild(bubbleChat);
+
+    // * Send socket event with `send-message` key
+    socketIO.emit('send-message', input.value);
+
     inputMessage.value = '';
-})
+});
+
+// * listen/subscribe the socket event by `new-message` key
+socketIO.on('new-message', (data) => {
+    const bubbleChat = createBubbleChat(data);
+    bubbleChat.classList.add('chat-right');
+    displayChat.appendChild(bubbleChat);
+});
--- server.js
+++ server.js
@@ -1,6 +1,22 @@
+const http = require('http');
+const { Server } = require('socket.io');
 const express = require('express');
+
 const app = express();
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
 
 app.use(express.static('public'));
 
-app.listen(3000);
+io.on('connection', (socket) => {
+    console.log('socket connected!');
+
+    // * listen/subscribe the socket event by `send-message` key
+    socket.on('send-message', (data) => {
+        // * do broadcast the data with different key
+        console.log('Get message from FE');
+        socket.broadcast.emit('new-message', data);
+    });
+});
+
+httpServer.listen(3000);
